c5a5e09fe3e9d46fe7e0a4ba15e109fb382a96d7
Sravan S <sravans2011@gmail.com>
2018-01-22 13:29:08 +0900

    reduce errors by checking for instance

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ export default class Chart extends Visualization {
 
   refresh() {
     try {
-      this.chartInstance.setSize(this.targetEl.width())
+      this.chartInstance && this.chartInstance.setSize(this.targetEl.width())
     } catch (e) {
       console.warn(e)
     }
